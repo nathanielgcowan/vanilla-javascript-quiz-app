4f5c2d37d9cf4a6616ae1d7eba5ed0572805da0d
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-07-19 18:40:13 -0500

    push

--- script.js
+++ script.js
@@ -6,15 +6,37 @@
     * BONUS: The alert (has the id `alert`)
 */
 const quizForm = document.getElementById("quiz-form");
-const answer = document.querySelectorAll(".answer")
+const answers = Array.from(document.querySelectorAll(".answer"))
 const questions = document.querySelectorAll("question-item")
 const alert = document.getElementById("alert")
 
 // TODO: 3. Create a submit event listener for the form that does the following.
+quizForm.addEventListener("submit", e => {
 //    1. Prevent the default behaviour
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
+    e.preventDefault();
 //    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
+    questions.forEach(questionItem => {
+      questionItem.classList.add("incorrect")
+      questionItem.classList.remove("correct")
+    })
+//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
+  const checkedAnswers = answers.filter(answer => answer.checked)
+  console.log(checkedAnswers)
+//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
+  answers.forEach(answer => {
+    const isCorrect = answer.value === "true"
+    const questionItem = answer.closest('.question-item')
+    
+    if (isCorrect) {
+      questionItem.classList.add("correct")
+      questionItem.classList.remove("incorrect")
+    } else {
+//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
+      questionItem.classList.add("incorrect")
+      questionItem.classList.remove("correct")
+    }
+  })
+//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
 //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+
+})
